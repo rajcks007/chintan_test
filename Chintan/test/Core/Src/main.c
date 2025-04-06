072c9310941c667a73a84390f07a22e859fd268d
rajcks_007 <rajchoksi97@gmail.com>
2025-04-06 12:59:44 +0200

    new

--- Chintan/test/Core/Src/main.c
+++ Chintan/test/Core/Src/main.c
@@ -112,11 +112,11 @@ int main(void)
 	  adc_values = HAL_ADC_GetValue(&hadc1);
 
 	  if(adc_values < 3000){
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
 		  data_1 = 1;
 	  }
 	  	else {
-	  		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+	  		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
 	  		data_1 = 0;
 	  		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 	  	}
@@ -296,7 +296,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|LD2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, efuse_EN_Pin|Buck_Boost_EN_Pin, GPIO_PIN_SET);
@@ -310,12 +310,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pins : PA1 LD2_Pin */
+  GPIO_InitStruct.Pin = GPIO_PIN_1|LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : efuse_EN_Pin Buck_Boost_EN_Pin */
   GPIO_InitStruct.Pin = efuse_EN_Pin|Buck_Boost_EN_Pin;
